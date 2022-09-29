1866c40e40c2faf1f9b10dfcebd9d7ad07aea2bf
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-09-29 19:36:25 +0200

    Update userController with registerUser function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,50 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+
 // @description Register new user
 // @route   POST /api/users
 // @access public
 
-const registerUser = (req, res) => {
-	res.json({ message: "Register User" });
-};
+const registerUser = asyncHandler(async (req, res) => {
+	const { name, email, password } = req.body;
+
+	if (!name || !email || !password) {
+		res.status(400);
+		throw new Error("Please add all fields");
+	}
+
+	// Check if user exists
+	const userExist = await User.findOne({ email });
+
+	if (userExist) {
+		res.status(400);
+		throw new Error("User already exist");
+	}
+
+	// Hash password
+	const salt = await bcrypt.genSalt(10);
+	const hashedPassword = await bcrypt.hash(password, salt);
+
+	// Create user
+	const user = User.create({
+		name,
+		email,
+		password: hashedPassword,
+	});
+
+	if (user) {
+		res.status(201).json({
+			_id: user.id,
+			name: user.name,
+			email: user.email,
+		});
+	} else {
+		res.status(400);
+		throw new Error("Invalid user data");
+	}
+});
 
 // @description Authenticate a user
 // @route   POST /api/users/login
