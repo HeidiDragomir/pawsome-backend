8748e2a5e69550e5db6141ff65e964cc7b2be282
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-09-29 18:42:58 +0200

    Update with userRoutes

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
-const connectDB = require("./config/db");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
 
 connectDB();
 
@@ -9,6 +12,12 @@ const app = express();
 
 // Parse json objects
 app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("API is running.");
+});
+
+app.use("/api/users", userRoutes);
 
 // Create a server
 const port = process.env.PORT || 3000;
