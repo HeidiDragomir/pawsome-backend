b78bd25990673d11d4e886924e638ec5c44624f4
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-09-30 16:51:38 +0200

    Create petModel

---
+++ models/petModel.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const petSchema = mongoose.Schema(
+	{
+		petName: {
+			type: String,
+			required: [true, "Please add a name"],
+		},
+		gender: {
+			type: String,
+			required: [true, "Please add a gender"],
+		},
+		age: {
+			type: Number,
+			required: [true, "Please add an age"],
+		},
+		size: {
+			type: String,
+			required: [true, "Please add a size"],
+		},
+		about: {
+			type: String,
+			required: [true, "Please add some info"],
+			maxlength: [1000, "Character can not be more than 1000 characters"],
+		},
+		photo: {
+			type: String,
+			required: [true, "Please add a photo"],
+		},
+		place: {
+			type: String,
+			required: [true, "Please add a place"],
+		},
+	},
+	{
+		timestamps: true,
+	}
+);
+
+export default mongoose.model("Pet", petSchema);
