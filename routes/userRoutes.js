6d91d79cbb9119a8692d59098f93ad815815b03e
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-09 22:09:20 +0200

    Update with admin routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,22 +3,41 @@ import {
 	registerUser,
 	loginUser,
 	profileUser,
+	updateProfileUser,
 	createAboutMe,
 	getAboutMe,
-	updateAboutMe
+	updateAboutMe,
+	registerUserAdmin,
+	getUsers,
+	getUserById,
+	updateUser,
+	deleteUser,
 } from "../controllers/userController.js";
 
-import { protect } from "../middleware/authMiddleware.js";
+import { protect, isAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/").post(registerUser);
+router.route("/").post(registerUser).get(protect, isAdmin, getUsers);
+
 router.post("/login", loginUser);
-router.get("/profile", protect, profileUser);
+router
+	.route("/profile")
+	.get(protect, profileUser)
+	.put(protect, updateProfileUser);
+
 router
 	.route("/profile/aboutme")
 	.post(protect, createAboutMe)
 	.get(protect, getAboutMe)
 	.put(protect, updateAboutMe);
+
+router.route("/admin").post(protect, isAdmin, registerUserAdmin);
+
+router
+	.route("/:id")
+	.get(protect, isAdmin, getUserById)
+	.put(protect, isAdmin, updateUser)
+	.delete(protect, isAdmin, deleteUser);
 
 export default router;
