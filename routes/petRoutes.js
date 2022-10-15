9cbbc26ac3608fd9842c0680be195c5317bbc7e3
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-10-15 12:56:26 +0200

    Update with getPetById route

--- routes/petRoutes.js
+++ routes/petRoutes.js
@@ -3,6 +3,7 @@ import {
 	createPet,
 	deletePet,
 	getPets,
+	getPetById,
 	getMyPets,
 	updatePet,
 } from "../controllers/petController.js";
@@ -13,6 +14,8 @@ const router = express.Router();
 router.get("/", getPets);
 
 router.route("/mypets").get(protect, getMyPets).post(protect, createPet);
+
+router.get("/:id", protect, getPetById);
 
 router.route("/mypets/:id").put(protect, updatePet).delete(protect, deletePet);
 
